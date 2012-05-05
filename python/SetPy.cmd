9a6daafb492a61397791bda5c86ca22dfc23fbcb
unknown <Dan@Dan-PC.(none)>
2012-05-05 13:26:46 +0800

    Remove the useless comment which block function

--- python/SetPy.cmd
+++ python/SetPy.cmd
@@ -4,7 +4,6 @@ REM Ver = 2	set Python27
 REM		  3	set Python32
 REM		  * echo current Path 
 
-The simplest expression
 @echo off
 if %1 == 2 set Path=%Path%;C:\Python27
 if %1 == 3 set Path=%Path%;C:\Python32
