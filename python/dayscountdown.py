d80b4b778cb6d64fa0a781a382f8178ef6691e14
unknown <Dan@Dorise-PC.(none)>
2012-05-19 17:01:14 +0800

    datetime handling
    A jasvascript to calculate the lunar calandar

---
+++ python/dayscountdown.py
@@ -0,0 +1,15 @@
+﻿#coding:utf-8
+
+from datetime import date
+
+MONTH = 5
+DAY = 25
+
+today = date.today()
+birthday = date(today.year, MONTH, DAY)
+if birthday < today:
+    birthday = birthday.replace(year = today.year + 1)
+time_to_birthday = abs(birthday - today)
+print time_to_birthday.days
+
+print birthday.isoweekday()
