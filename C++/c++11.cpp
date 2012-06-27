37d7255c6d7c0236edcd95be3d6917ea2c01c835
elixir67 <lind@SHACNG028WC4Y.ads.autodesk.com>
2012-06-27 17:32:59 +0800

    Add c++11.cpp to try for_each Add SetVC10x86.bat to setup envrionment directly

---
+++ C++/c++11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+   char s[]="Hello World!";
+   int Uppercase = 0; //modified by the lambda
+   for_each(s, s+sizeof(s), [&Uppercase] (char c) {
+    if (isupper(c))
+     Uppercase++;
+    });
+    cout<< Uppercase<<" uppercase letters in: "<< s<<endl;
+}
