2e451dd8212fd80e6819b820f0a71aca10c10792
unknown <lind@SHACNG028WC4Y.ads.autodesk.com>
2012-10-23 17:13:31 +0800

    Compute the number of 1 from 1 to number N for each digital.

---
+++ C++/NumberOf1.cpp
@@ -0,0 +1,47 @@
+// NumberOf1.cpp : Defines the entry point for the console application.
+//
+
+// #include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int NumberOf1(int n)
+{
+    int iFactor = 1;
+    int iCount = 0;
+    while(n/iFactor != 0)
+    {
+        int low = n - (n/iFactor) * iFactor;
+        int current = (n/iFactor) % 10;
+        int high = (n/iFactor) / 10;
+        switch(current)
+        {
+            case 0:
+                iCount += high * iFactor;
+                break;
+            case 1:
+                iCount += high * iFactor + (low + 1);
+                break;
+            default:
+                iCount += (high + 1) * iFactor;
+                break;
+        }
+        iFactor *= 10;
+    }
+    return iCount;
+}
+
+void Test1(int n,int expected)
+{
+    if(expected == NumberOf1(n))
+        cout << "Test1 passed!" << endl;
+    else
+        cout << "Test1 failed!" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    Test1(21345,18821);
+	return 0;
+}
+
