6642f29af6deae60cc4829d8cae9b712bf5db102
unknown <lind@SHACNG028WC4Y.ads.autodesk.com>
2012-11-07 18:04:05 +0800

    Add KthNodeFromEnd project

---
+++ C++/Questions/KthNodeFromEnd/KthNodeFromEnd.cpp
@@ -0,0 +1,16 @@
+// KthNodeFromEnd.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "..\Utilities\List.h"
+
+ListNode * FindKthToTail(ListNode * pHead, unsigned int k)
+{
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
