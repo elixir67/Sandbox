711e6deec623bf62aa55589479ca532a6d742a9b
unknown <lind@SHACNG028WC4Y.ads.autodesk.com>
2012-12-10 13:43:01 +0800

    Fix one issue of StringToInt, not finished yet, it still has bug.

--- C++/Questions/StringToInt/StringToInt.cpp
+++ C++/Questions/StringToInt/StringToInt.cpp
@@ -12,15 +12,30 @@ bool StringToInt(const char * str, int & val)
         return false;
 
     int temp = 0;
+    bool isNegative = false;
+
+    // consider the negative number
+    if(*str == '-')
+    {
+        isNegative = true;
+        ++str;
+    }
+    else if(*str == '+')
+    {
+        isNegative = false;
+        ++str;
+    }
+
     while('\0' != *str)
     {
         if(*str >= '0' && *str <= '9')
             temp = temp * 10 + (*str - '0');
         else
             return false;
+        ++str;
     }
 
-    val = temp;
+    val = isNegative? temp*(-1): temp;
     return true;
 }
 
@@ -28,13 +43,13 @@ void Test(const char * str, int expect)
 {
     int result = 0;
     if(StringToInt(str,result) && expect == result)
-        std::cout << "Convert succeed with result " + result << endl;
+        cout << "Convert succeed with result " << result << endl;
     else
-        std::cout << "Convert fail!"  + result << endl;
+        cout << "Convert fail!"  + result << endl;
 }
 
 void main()
 {
     Test("1234", 1234);
-    // Test("-1234", -1234);
+    Test("-1234", -1234);
 }
