5003b4defbf81b4a274efedd5c6b23f3d66598c8
EliteBook <EliteBook@elixir>
2012-12-16 16:26:19 +0800

    Initial SymmetricalStr project not implemented yet

---
+++ C++/Questions/53_SymmetricalStr/53_SymmetricalStr.cpp
@@ -0,0 +1,25 @@
+// 53_SymmetricalStr.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+// returns the maximum sub string which is symmetrical
+// e.g. google return goog
+
+void SymmetricalStr(char * pStr, char * pBegin, char * pEnd)
+{
+	if(!pStr)
+	{
+		pBegin = pEnd = NULL;
+		return;
+	}
+
+	// 
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
