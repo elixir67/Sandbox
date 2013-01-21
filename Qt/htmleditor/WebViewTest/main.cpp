665c72eae9726c4b00537ec895ec9b8271deb8ed
unknown <lind@SHACNG028WC4Y.ads.autodesk.com>
2013-01-21 09:28:06 +0800

    Commit left files

--- Qt/htmleditor/WebViewTest/main.cpp
+++ Qt/htmleditor/WebViewTest/main.cpp
@@ -7,8 +7,18 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     WebView b;
+
+
+    b.load(QUrl("http://www.massculturalcouncil.org/facilities/images/Sargent_House.jpg")); // works
     b.show();
 
-    b.load(QUrl("http://www.baidu.com/img/baidu_sylogo1.gif")); // works
+
+    QSize s = b.sizeHint();
+    QSizePolicy p = b.sizePolicy();
+    b.setBaseSize(100, 100);
+
+    //p.setHorizontalPolicy(QSizePolicy::ShrinkFlag);
+    b.setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+
     return a.exec();
 }
