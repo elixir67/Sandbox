94c16caaf55f7f3d51a11e28a8b989d32b7a44b8
unknown <Dan@Dan.(none)>
2013-05-31 23:45:23 +0800

    Exercise boost to create thread

--- C++/Boost/Boost/Ch12_Thread.cpp
+++ C++/Boost/Boost/Ch12_Thread.cpp
@@ -7,15 +7,36 @@
 //#include <libs/thread/src/win32/thread.cpp>
 //#include <libs/thread/src/win32/tss_dll.cpp>
 //#include <libs/thread/src/win32/tss_pe.cpp>
-
+#include "stdafx.h"
 #include <boost/thread.hpp>
+#include <boost/atomic.hpp>
+#include <boost/ref.hpp>
 
-void testThread()
+void testThreadSleep()
 {
 	boost::this_thread::sleep(boost::posix_time::seconds(2));
 }
 
+boost::mutex io_mu;
+
+void printing(boost::atomic_int & x, const string &str)
+{
+	for(int i = 0; i < 5; ++i)
+	{
+		boost::mutex::scoped_lock lock(io_mu);
+		cout << str << ++x << endl;
+	}
+}
+
+void testCreateThreadWithParameter()
+{
+	boost::atomic_int x;
+
+	boost::thread(printing, boost::ref(x), "hello");
+	boost::thread(printing, boost::ref(x), "boost");
+}
 void testCh12()
 {
-	testThread();
+	testCreateThreadWithParameter();
+	testThreadSleep();
 }
