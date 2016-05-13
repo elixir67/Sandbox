71857a385fb09fe968170f2ce4fd60af3487d808
Dan Lin <Dan.Lin@blackboard.com>
2016-05-13 14:43:09 +0800

    Refactor code

--- Java/LearnHelper/src/main/java/LearnHelper.java
+++ Java/LearnHelper/src/main/java/LearnHelper.java
@@ -1,17 +1,8 @@
-//package src.main.java;
-
-//import org.apache.commons.codec.binary.Base64;
-//import org.jsoap.*;
-
-import org.apache.log4j.Logger;
 import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 import java.util.Properties;
 
 
@@ -20,88 +11,96 @@ import java.util.Properties;
  */
 public class LearnHelper {
 
-    private static org.apache.log4j.Logger log = Logger.getLogger(LearnHelper.class);
+    private String host;
+    private int usersNum;
+    private String config;
 
-    public static void main(String[] args)
+    public LearnHelper(String config)
     {
-        GetLearnVersion();
+        this.config = config;
     }
 
-    private static void GetConfigInfo()
+    public String GetHost()
+    {
+        return host;
+    }
+
+    public int GetUsersNum()
+    {
+        return usersNum;
+    }
+
+    public void ParseConfigInfo()
     {
         Properties prop = new Properties();
-        String propFileName = "learn-pf-saas01_user_config.properties";
+        //String propFileName = "learn-pf-saas01_user_config.properties";
+        String propFileName = config;
         try {
             InputStream inputStream = LearnHelper.class.getClassLoader().getResourceAsStream(propFileName);
             if (inputStream != null) {
                 prop.load(inputStream);
 
                 // get the property value and print it out
-                String host = prop.getProperty("hosts");
-                log.debug(host);
-
-
+                host = prop.getProperty("hosts");
                 String users = prop.getProperty("test.threads");
-                log.debug(users);
-
+                usersNum = Integer.parseInt(users);
             }
             else {
-                log.debug("Cannot locate "+propFileName);
+                System.out.println("Cannot locate " + propFileName);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void GetLearnVersion() {
+    public static String GetLearnVersion(String host) {
         String userId = "administrator";
         String password = "IzCClI-1C0jv4A";
-        //String encodedPwd = new String(Base64.encodeBase64(password.getBytes()));
         try {
-            String host = "https://learn-pf-saas01.blackboard.com";
-            String loginUrl = host + "/webapps/login/";
+            //String host = "https://learn-pf-saas01.blackboard.com";
+            String loginUrl = "https://" + host + "/webapps/login/";
 
             Response res = Jsoup.connect(loginUrl)
                     .header("Content-Type", "application/x-www-form-urlencoded")
-//                    .header("Cache-Control", "no-cache")
-//                    .header("'Connection'", "keep-alive")
                     .data("user_id", userId)
                     .data("password", password)
                     .data("login", "Sign In")
                     .data("new_loc", "")
                     .data("action", "login")
-//                    .data("encoded_pw", password)
                     .method(Method.POST)
                     .execute();
 
-            Document doc1 = res.parse();
-
-            log.debug(res.hasHeader("X-Blackboard-product"));
             String productName = res.header("X-Blackboard-product");
-            log.debug(productName);
+            //log.debug(productName); // sample output: Blackboard Learn &#8482; 3000.3.0-rel.45+a7bf6a1
 
             String version = productName.substring(productName.indexOf("; ")+2);
-            log.debug(version);
+            //log.debug(version);
 
-            //String adminUrl = host + "/ultra/admin/";
-
-            // How to send REST request?
-//            String adminUrl = host + "/learn/api/v1/utilities/systemInfo";
-//            //This will get you cookies
-//            Map<String, String> loginCookies = res.cookies();
-//
-//            //And this is the easiest way I've found to remain in session
-//            Response res2 = Jsoup.connect(adminUrl)
-//                    .header("Content-Type", "Mimetype=application/json;charset=UTF-8")
-//                    .cookies(loginCookies)
-//                    .method(Method.GET)
-//                    .execute();
-//
-//            log.debug(res2.statusCode());
-//            log.debug(res2.body());
+            return version;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return null;
     }
+
+//    public static void GetRESTRequest() {
+//        //String adminUrl = host + "/ultra/admin/";
+//
+//        // How to send REST request?
+////            String adminUrl = host + "/learn/api/v1/utilities/systemInfo";
+////            //This will get you cookies
+////            Map<String, String> loginCookies = res.cookies();
+////
+////            //And this is the easiest way I've found to remain in session
+////            Response res2 = Jsoup.connect(adminUrl)
+////                    .header("Content-Type", "Mimetype=application/json;charset=UTF-8")
+////                    .cookies(loginCookies)
+////                    .method(Method.GET)
+////                    .execute();
+////
+////            log.debug(res2.statusCode());
+////            log.debug(res2.body());
+//    }
 }
---
+++ Java/LearnHelper/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;
+
+/**
+ * Created by dlin on 5/13/16.
+ */
+public class Main {
+
+    private static org.apache.log4j.Logger log = Logger.getLogger(Main.class);
+    public static void main(String[] args)
+    {
+        LearnHelper learnHelper = new LearnHelper("learn-pf-saas01_user_config.properties");
+        learnHelper.ParseConfigInfo();
+        log.debug(learnHelper.GetLearnVersion(learnHelper.GetHost()));
+        log.debug(learnHelper.GetUsersNum());
+    }
+}
