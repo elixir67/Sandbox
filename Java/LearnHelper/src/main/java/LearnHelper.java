eadb6dc283ec9a02c8583e1e43f1fd56dbe4e09b
Dan Lin <Dan.Lin@blackboard.com>
2016-05-13 13:34:15 +0800

    Parse the properties file
    Overwrite the log file as well.

--- Java/LearnHelper/src/main/java/LearnHelper.java
+++ Java/LearnHelper/src/main/java/LearnHelper.java
@@ -2,13 +2,17 @@
 
 //import org.apache.commons.codec.binary.Base64;
 //import org.jsoap.*;
-import org.jsoup.Connection;
-import org.jsoup.Connection.*;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
 
 
 /**
@@ -20,15 +24,47 @@ public class LearnHelper {
 
     public static void main(String[] args)
     {
+        GetLearnVersion();
+    }
+
+    private static void GetConfigInfo()
+    {
+        Properties prop = new Properties();
+        String propFileName = "learn-pf-saas01_user_config.properties";
+        try {
+            InputStream inputStream = LearnHelper.class.getClassLoader().getResourceAsStream(propFileName);
+            if (inputStream != null) {
+                prop.load(inputStream);
+
+                // get the property value and print it out
+                String host = prop.getProperty("hosts");
+                log.debug(host);
+
+
+                String users = prop.getProperty("test.threads");
+                log.debug(users);
+
+            }
+            else {
+                log.debug("Cannot locate "+propFileName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void GetLearnVersion() {
         String userId = "administrator";
         String password = "IzCClI-1C0jv4A";
         //String encodedPwd = new String(Base64.encodeBase64(password.getBytes()));
         try {
             String host = "https://learn-pf-saas01.blackboard.com";
-            String loginUrl = host + "/webapps/login";
+            String loginUrl = host + "/webapps/login/";
 
-            Connection.Response res = Jsoup.connect(loginUrl)
+            Response res = Jsoup.connect(loginUrl)
                     .header("Content-Type", "application/x-www-form-urlencoded")
+//                    .header("Cache-Control", "no-cache")
+//                    .header("'Connection'", "keep-alive")
                     .data("user_id", userId)
                     .data("password", password)
                     .data("login", "Sign In")
@@ -39,25 +75,33 @@ public class LearnHelper {
                     .execute();
 
             Document doc1 = res.parse();
-//            System.out.println(res.hasHeader("X-Blackboard-product"));
-//            System.out.println(res.header("X-Blackboard-product"));
 
             log.debug(res.hasHeader("X-Blackboard-product"));
-            log.debug(res.header("X-Blackboard-product"));
+            String productName = res.header("X-Blackboard-product");
+            log.debug(productName);
 
-//            String adminUrl = host + "/ultra/admin";
+            String version = productName.substring(productName.indexOf("; ")+2);
+            log.debug(version);
+
+            //String adminUrl = host + "/ultra/admin/";
+
+            // How to send REST request?
+//            String adminUrl = host + "/learn/api/v1/utilities/systemInfo";
 //            //This will get you cookies
 //            Map<String, String> loginCookies = res.cookies();
 //
-//
-//
 //            //And this is the easiest way I've found to remain in session
-//            Document doc2 = Jsoup.connect(adminUrl)
+//            Response res2 = Jsoup.connect(adminUrl)
+//                    .header("Content-Type", "Mimetype=application/json;charset=UTF-8")
 //                    .cookies(loginCookies)
-//                    .get();
+//                    .method(Method.GET)
+//                    .execute();
+//
+//            log.debug(res2.statusCode());
+//            log.debug(res2.body());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("hello world");
     }
 }
